e6b53d96823f9c8be918f725068af7810dc72ea3
v0agent <it+v0agent@vercel.com>
2026-06-09 17:10:19 +0000

    Initial commit from v0

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { DesignaliCreative } from "@/components/creative" 
+
+ 
+export default function Home() {
+   
+  return (
+     
+      <main className="overflow-hidden">  
+        
+          <DesignaliCreative />
+  
+      </main>
+     
+  )
+}
